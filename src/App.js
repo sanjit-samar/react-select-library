5b92621e4b9ab656cbf1a6d787067993862c5dd6
Sanjit Samar <sanjitsamar07@gmail.com>
2025-12-15 10:21:43 +0000

    added feature

--- src/App.js
+++ src/App.js
@@ -4,9 +4,10 @@ import Select from "react-select";
 
 export default function App() {
   const [product, setProduct] = useState([]);
+  const [selectedProducts, setSelectedProducts] = useState([]);
 
   async function fetchProduct() {
-    const data = await fetch("https://dummyjson.com/products?limit=5");
+    const data = await fetch("https://dummyjson.com/products?limit=25");
     const json = await data.json();
     setProduct(json.products);
   }
@@ -19,13 +20,30 @@ export default function App() {
     return product.map((item) => ({
       label: item.title,
       value: item.id,
+      thumbnail: item.thumbnail
     }));
   }, [product]);
+
 
   return (
     <div className="App">
       <h1>Hello </h1>
-      <Select name="color" options={filterOption} isMulti />
+      <Select
+        value={selectedProducts}
+        onChange={setSelectedProducts}
+        name="color"
+        options={filterOption}
+        isMulti
+      />
+      <div className="Container">
+      {selectedProducts.map((txt)=> (
+        <div key={txt.id} className="Product"> 
+            <img src={txt?.thumbnail} alt="No Images" />
+            <p style={{textAlign: 'left'}}>{txt.label}</p>
+           
+        </div>
+      ))}
+      </div>
     </div>
   );
 }
